aff0c537e48be3d2215d86e5e4767dc6afc42600
MatthewHufft <matthufft777@gmail.com>
2020-11-10 12:42:23 -0700

    added app src

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from "vue";
-const app = createApp({
 
+const app = createApp({
+  data: () => ({
+    modalOpen: false
+  }),
+  methods: {
+    toggleModalState(){
+      this.modalOpen = !this.modalOpen;
+    }
+  }
 });
 
 app.mount("#app");
